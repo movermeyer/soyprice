95347d62f72136686ff4331b2e619c6c50a2e912
eloy <eloy.colell@gmail.com>
2014-11-09 21:12:06 -0300

    It added some weights to the forecast function.

--- soyprice/statistic.py
+++ soyprice/statistic.py
@@ -1,9 +1,13 @@
 from numpy import polyfit, poly1d, sqrt
+from math import log, e
 
 def forecast(x, y, future_x):
-    fit = polyfit(x, y, 6)
+    weights = map(lambda xi: (xi -x[0])/float(x[-1] - x[0]), x) 
+    print weights
+    fit = polyfit(x, y, 6, w=weights)
     fx = poly1d(fit)
     estimated = map(fx, x)
     rmse = sqrt(sum(map(lambda (e, x): (e - x) ** 2, zip(estimated, y)))
                 /len(estimated))
+    print fx(future_x)
     return fx(future_x), rmse, estimated, fx
