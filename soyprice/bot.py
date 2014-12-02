ac09ec7d479bea0fcbf427c5c426f4d9d2d0ac0e
eloy <eloy.colell@gmail.com>
2014-12-01 23:31:35 -0300

    It refactor the graph function into the grapher.py file.

--- soyprice/bot.py
+++ soyprice/bot.py
@@ -5,12 +5,9 @@ from twython import Twython, TwythonError
 import datetime
 from scraper import get_prices, get_chicago_price, get_days, get_next_workable_day, date_to_int, get_dollars
 from statistic import forecast
-import pylab as pl
-from PIL import Image
-from StringIO import StringIO
 import numpy as np
 import database as db
-
+from grapher import graph
 
 
 APP_KEY = 'rogbcg4oUIEHGh35kxMVGAf2k'
@@ -26,44 +23,6 @@ def tweet(status, image):
     twitter.update_status_with_media(media=photo, status=template % status)
 
 
-def graph(x, y, fix, next_x, next_y, dollars, fix_d, next_d_x, next_d_y, rmse, weights):
-    data = filter(lambda d: d[1], zip(x, y))
-    x, y = zip(*data)
-    x = map(date_to_int, x)
-    border = 2
-    ratio = 100
-    x_values = list(x) + [next_x]
-    y_values = list(y) + [next_y]
-    limits = (min(x_values) - border,
-              max(x_values) + border,
-              min(y_values) - border * ratio,
-              max(y_values) + border * ratio)
-    pl.figure(figsize=(8, 4), dpi=100)
-    pl.title('Soja puerto San Martin por @limiear')
-    ax = pl.subplot(1, 1, 1)
-    ax.axis(limits)
-    ax.scatter(x, y, marker=".", linewidth=0.5)
-    w_s = lambda w: 1/(w if w > 0 else 0.001)
-    ax.plot(x, map(lambda (x, w): x - (rmse * w_s(w)), zip(fix, weights)),
-            color="green", linewidth=1.0, linestyle="--",)
-    ax.plot(x, fix, color="red", linewidth=1.0, linestyle="-",)
-    ax.plot(x, map(lambda (x, w): x + (rmse * w_s(w)), zip(fix, weights)),
-            color="green", linewidth=1.0, linestyle="--",)
-    ax.plot([next_x], [next_y], color="red", marker="o")
-    pl.xticks([], 0, endpoint=True)
-    pl.xlabel("ventana de %i dias previos" % (x[-1] + 1 - x[0]), fontsize=10)
-    pl.ylabel('AR$', fontsize=10)
-    ax.yaxis.tick_right()
-    ax.yaxis.set_label_position("right")
-    # bx = pl.subplot(2, 1, 2)
-    # bx.scatter(*zip(*dollars))
-    # pl.plot(x, fix_d, color="red", linewidth=1.0, linestyle="-",)
-    # pl.plot([next_d_x], [next_d_y], color="red", marker="o")
-    # bx.yaxis.tick_right()
-    # bx.yaxis.set_label_position("right")
-    filename = "graph.png"
-    pl.savefig(filename, dpi=100)
-    return filename
 
 
 def step():
