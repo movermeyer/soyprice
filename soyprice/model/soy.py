c55ad2d6a6d9a7c26c005cba719f30fbdcb82d3a
eloy <eloy.colell@gmail.com>
2014-12-27 11:17:50 -0300

    It improved the afascl scrap_date method.

--- soyprice/model/soy.py
+++ soyprice/model/soy.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup as beautifulsoup
 from itertools import chain
 import re
 import datetime
+from translate import Translator
 
 
 class Soy(Variable):
@@ -36,15 +37,12 @@ class Afascl(Soy):
     def __init__(self, cache):
         super(Afascl, self).__init__(cache)
         self.name += '/afascl'
+        self.translator = Translator(from_lang="es", to_lang="en")
 
-    def scrap_date(self, date, place):
-        if date == self.today:
-            url = 'http://afascl.coop/afadiario/home/diario.php'
-        else:
-            date_str = date.strftime('%d-%m-%Y')
-            url = ('http://diario.afascl.coop/afaw/afa-tablas/dispo.do?'
-                   'tk=1414884447433&mode=get&fecha=%s&_=' % date_str)
-        page = beautifulsoup(self.request(url))
+    def should_scrap(self, date):
+        return date == self.today
+
+    def obtain_prices(self, page, place):
         rows = page.select('tr')
         fix_string = lambda x: x.lower().strip(' \.\-')
         text = (lambda x: fix_string(x.encode('utf-8')
@@ -61,9 +59,24 @@ class Afascl(Soy):
         soy_with_download = lambda x: ('soja' in x[0]
                                        and 'con descarga' in x[4]
                                        and x[2] > 0 and place in x[1])
-        prices = map(lambda x: x[2],
-                     filter(soy_with_download, prices))
-        return prices
+        return map(lambda x: x[2],
+                   filter(soy_with_download, prices))
+
+    def scrap_date(self, date, place):
+        if date == self.today:
+            url = 'http://afascl.coop/afadiario/home/diario.php'
+        else:
+            date_str = date.strftime('%d-%m-%Y')
+            url = ('http://diario.afascl.coop/afaw/afa-tablas/dispo.do?'
+                   'tk=1414884447433&mode=get&fecha=%s&_=' % date_str)
+        page = beautifulsoup(self.request(url))
+        has_price = True
+        if date == self.today:
+            today = page.select('.preciosdiario span')[0].text.split('\r\n')[0]
+            today = self.translator.translate(today.lower())
+            today = datetime.datetime.strptime(today, '%A %B %d, %Y').date()
+            has_price = (today == self.today)
+        return self.obtain_prices(page, place) if has_price else []
 
 
 class SanMartin(Afascl):
