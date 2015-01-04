ecd27ef6594f21c186ddbf6fb8522ef702723ed8
eloy <eloy.colell@gmail.com>
2015-01-04 15:35:03 -0300

    It improved the coverage over the scraper module (issue #8). It cleaned unused code from the scraper module.

--- tests/scraper_test.py
+++ tests/scraper_test.py
@@ -8,13 +8,8 @@ class TestScraper(unittest.TestCase):
 
     def setUp(self):
         self.day = datetime.date(2014,10,8)
-        self.prices = [(datetime.date(2014, 10, 8), 2260.0)]
         self.days = [datetime.date(2014, 10, 8) - datetime.timedelta(days=x)
                      for x in range(0, 15)]
-        self.cache = database.open()
-
-    def tearDown(self):
-        database.close(self.cache)
 
     def test_get_days(self):
         days = scraper.get_days(datetime.datetime(2014, 10, 8))
@@ -25,6 +20,10 @@ class TestScraper(unittest.TestCase):
         self.assertEquals(day, datetime.date(2014, 10, 9))
         day = scraper.get_next_workable_day(datetime.date(2014, 10, 10))
         self.assertEquals(day, datetime.date(2014, 10, 13))
+
+    def test_date_to_int(self):
+        number = scraper.date_to_int(self.day)
+        self.assertEquals(number, 735514)
 
 
 if __name__ == '__main__':
