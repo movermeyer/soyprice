afa5e4ac850e947eb566b82bf25aabac37cecbc2
eloy <eloy.colell@gmail.com>
2016-03-21 00:35:32 -0300

    Append petroleum.py (WTI) to the variables folder.

---
+++ soyprice/variables/bcra_reserves.py
@@ -0,0 +1,37 @@
+from variables.core import app, db, get_var, request, Change
+from datetime import datetime
+
+
+# http://www.bcra.gov.ar/Estadisticas/estprv010001.asp
+@app.run_every("day", "10:30")
+def update_dollars_ratios():
+    url = ("http://www.ambito.com/economia/mercados/monedas/dolar/"
+           "x_dolar_get_grafico.asp?ric={:}&tipo={:}")
+    dollar_vars = {
+        "ARSB=": {
+            "name": u"dollar/blue",
+            "description": u"Dolar blue en Argentina",
+            "reference": u"ARS/USD"
+        },
+        "ARSSCBCRA": {
+            "name": u"dollar/bcra",
+            "description": u"Dolar del Banco Central de la Republica Argentina",
+            "reference": u"ARS/USD"
+        }
+    }
+    for k, v in dollar_vars.items():
+        variable = get_var(**v)
+        lapse = "ww" if variable.changes.count() else "yyyy"
+        composed_url = url.format(k, lapse)
+        dollars = map(
+            lambda (d, v): (datetime.strptime(d, "%Y/%m/%d").date(), v),
+            eval(request(composed_url)))
+        dts = map(lambda ch: ch.moment, variable.changes.all())
+        if dts:
+            dollars = filter(lambda d: d[0] not in dts, dollars)
+        for d, v in dollars:
+            ch = Change(value=v, moment=d)
+            variable.changes.append(ch)
+            db.session.add(ch)
+        db.session.add(variable)
+        db.session.commit()
--- soyprice/variables/petroleum.py
+++ soyprice/variables/petroleum.py
@@ -1,34 +1,33 @@
 from variables.core import app, db, get_var, request, Change
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
-@app.run_every("day", "10:30")
-def update_dollars_ratios():
-    url = ("http://www.ambito.com/economia/mercados/monedas/dolar/"
-           "x_dolar_get_grafico.asp?ric={:}&tipo={:}")
-    dollar_vars = {
-        "ARSB=": {
-            "name": "dollar/blue",
-            "description": "Dolar blue en Argentina",
-            "reference": "ARS/USD"
-        },
-        "ARSSCBCRA": {
-            "name": "dollar/bcra",
-            "description": "Dolar del Banco Central de la Republica Argentina",
-            "reference": "ARS/USD"
+#@app.run_every("day", "12:30")
+dt = datetime.now() + timedelta(minutes=1)
+@app.run_every("day", dt.strftime("%H:%M"))
+def update_petroleum_wti_barrel_ratios():
+    url = ("http://www.ambito.com/economia/mercados/petroleo/"
+           "x_petroleo_get_grafico.asp?ric=1&"
+           "timeFrom={:}&timeTo={:}&tipo=libre")
+    begin = datetime(1984, 4, 12)
+    end = datetime.now()
+    petroleum_vars = {
+        "PETROLEUM": {
+            "name": "petroleum/wti",
+            "description": "Petroleo West Texas Intermediate",
+            "reference": "USD/barrel"
         }
     }
-    for k, v in dollar_vars.items():
+    for k, v in petroleum_vars.items():
         variable = get_var(**v)
-        lapse = "ww" if variable.changes.count() else "yyyy"
-        composed_url = url.format(k, lapse)
-        dollars = map(
+        begin = (variable.changes.order_by("moment desc").first().moment
+                 if variable.changes.count() else begin)
+        p = lambda dt: dt.strftime("%d/%m/%Y")
+        composed_url = url.format(p(begin), p(end))
+        prices = map(
             lambda (d, v): (datetime.strptime(d, "%Y/%m/%d").date(), v),
             eval(request(composed_url)))
-        dts = map(lambda ch: ch.moment, variable.changes.all())
-        if dts:
-            dollars = filter(lambda (d, v): d[0] not in dts, dollars)
-        for d, v in dollars:
+        for d, v in prices:
             ch = Change(value=v, moment=d)
             variable.changes.append(ch)
             db.session.add(ch)
