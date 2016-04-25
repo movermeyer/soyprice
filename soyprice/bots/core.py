1141de641d513b8e422d071400e3b46b3e6a16a7
eloy <eloy.colell@gmail.com>
2016-04-25 17:39:07 -0300

    The bots uses the variables.core functions to access the data throught the json API.

--- soyprice/bots/core.py
+++ soyprice/bots/core.py
@@ -1,22 +1,24 @@
-import models
 from datetime import datetime
+from variables.core import get_var
 
 
 class Variable(object):
 
     def __init__(self, name):
         self.name = name
-        self.variable = models.Variable.query.filter_by(name=name).first()
-        self.description = self.variable.description
-        if not self.description:
-            self.description = ''
-        self.reference = self.variable.reference
+        self.variable = get_var(name)
+        self.description = self.variable['description']
+        self.reference = self.variable['reference']
 
     @property
     def today(self):
         return datetime.now().date()
 
+    def to_date(self, moment):
+        return datetime.strptime(moment, "%Y-%m-%d").date()
+
     def get(self, date_list=[]):
-        changes = self.variable.changes.order_by("moment").all()
-        elements = filter(lambda ch: ch.moment in date_list, changes)
-        return map(lambda e: (e.moment, float(e.value)), elements)
+        changes = sorted(self.variable['changes'], key=lambda c: c["moment"])
+        elements = filter(lambda ch: ch['moment'] in date_list, changes)
+        return map(lambda e: (self.to_date(e['moment']), float(e['value'])),
+                   elements)
